55b174012b717e18d44b877f62b464d37bb2de2c
Hamza <Hfarah5618@gmail.com>
2025-09-03 15:25:11 -0500

    Initial Commit

---
+++ To-Do List App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To-Do List App
+//
+//  Created by Hamza Farah on 9/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To-Do List App/To_Do_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_List_AppApp.swift
+//  To-Do List App
+//
+//  Created by Hamza Farah on 9/3/25.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
